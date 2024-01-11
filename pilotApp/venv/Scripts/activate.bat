c41636e6f5fce744fd758f97cba1d6b11b71d72c
InteradDevs <73945173+interad@users.noreply.github.com>
2024-01-11 17:16:03 +0200

    reinstalled venv + djangoframw

--- pilotApp/venv/Scripts/activate.bat
+++ pilotApp/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\laragon\www\Django_RestApi_MsSQL\pilotApp\venv
+set VIRTUAL_ENV=C:\Users\Imac-Pro\Desktop\Django_RestApi_MsSQL\pilotApp\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
